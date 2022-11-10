ebaccdfc1e798504872cd7740f2b3ccd9db0f257
Sonja Eisner <01113143@edu.campus02.at>
2022-11-10 15:30:06 +0100

    add additional fields in Kind class

--- KindergartenWebServices/Models/Kind.cs
+++ KindergartenWebServices/Models/Kind.cs
@@ -3,31 +3,41 @@
     public class Kind
     {
         public int KindId { get; set; }
-        public int SVN { get; set; }
         public string Nachname { get; set; }
         public string Vorname { get; set; }
+        public string SVN { get; set; }
+        public int Geburtstag { get; set; }
+        public int Geburtsmonat { get; set; }
+        public int Geburtsjahr { get; set; }
 
-        public int ErzBerechtiger1SVN { get; set; }
+        public string HauptwohnsitzAdresse { get; set; }
+        public string HauptwohnsitzPLZ { get; set; }
+        public string HauptwohnsitzOrt { get; set; }
+
         public string ErzBerechtiger1Nachname { get; set; }
         public string ErzBerechtiger1Vorname { get; set; }
-        public int ErzBerechtiger2SVN { get; set; }
+        public string ErzBerechtiger1SVN { get; set; }
         public string ErzBerechtiger2Nachname { get; set; }
         public string ErzBerechtiger2Vorname { get; set; }
+        public string ErzBerechtiger2SVN { get; set; }
 
-        public string Adresse { get; set; }
-        public string PLZ { get; set; }
-        public string Ort { get; set; }
+        public string RechnungAdresse { get; set; }
+        public string RechnungPLZ { get; set; }
+        public string RechnungOrt { get; set; }
+        public string EmailRechnung { get; set; }
 
-        public int AnzahlGeschwister { get; set; }
+        public string Betreuungsart { get; set; }
+        public string Betreuungsumfang { get; set; }
 
         public float Familieneinkommen { get; set; }
+        public int AnzahlGeschwister { get; set; }
 
-        public string Leistung { get; set; }
 
+        public int Staffelstufe { get; set; }
         public float Betreuungsbeitrag { get; set; }
 
-        public string KontaktTelefon { get; set; }
-        public string KontaktEmail { get; set; }
+   
+        
 
     }
 }
