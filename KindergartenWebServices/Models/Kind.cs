dfd7ea650a06d02c9cb2a2d55ec81fb6ab894252
Sonja Eisner <01113143@edu.campus02.at>
2022-11-10 13:17:27 +0100

    add Kind model

---
+++ KindergartenWebServices/Models/Kind.cs
@@ -0,0 +1,33 @@
+﻿namespace KindergartenWebServices.Models
+{
+    public class Kind
+    {
+        public int KindId { get; set; }
+        public int SVN { get; set; }
+        public string Nachname { get; set; }
+        public string Vorname { get; set; }
+
+        public int ErzBerechtiger1SVN { get; set; }
+        public string ErzBerechtiger1Nachname { get; set; }
+        public string ErzBerechtiger1Vorname { get; set; }
+        public int ErzBerechtiger2SVN { get; set; }
+        public string ErzBerechtiger2Nachname { get; set; }
+        public string ErzBerechtiger2Vorname { get; set; }
+
+        public string Adresse { get; set; }
+        public string PLZ { get; set; }
+        public string Ort { get; set; }
+
+        public int AnzahlGeschwister { get; set; }
+
+        public float Familieneinkommen { get; set; }
+
+        public string Leistung { get; set; }
+
+        public float Betreuungsbeitrag { get; set; }
+
+        public string KontaktTelefon { get; set; }
+        public string KontaktEmail { get; set; }
+
+    }
+}
