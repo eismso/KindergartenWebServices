6528ee0f276f8597d807fa3ad9af5c32803968c2
Sonja Eisner <01113143@edu.campus02.at>
2022-11-19 00:42:48 +0100

    add KindController and Validators in Kind class

---
+++ KindergartenWebServices/Controllers/KindController.cs
@@ -0,0 +1,99 @@
+﻿using KindergartenWebServices.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KindergartenWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KindController : ControllerBase
+    {
+
+        static List<Kind> kindList;
+
+        static KindController()
+        {
+            kindList = new List<Kind>();
+        }
+
+        // GET: api/<Kind>
+        [HttpGet]
+        public List<Kind> Get()
+        {
+            return kindList;
+        }
+
+        // GET api/<Kind>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Kind gefunden = new Kind();
+            foreach (Kind k in kindList)
+            {
+                if (k.KindId == id)
+                {
+                    gefunden = k;
+                }
+                    
+            }
+
+            if (gefunden.KindId == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(gefunden);
+            }
+        }
+
+        // POST api/<Kind>
+        [HttpPost]
+        public IActionResult Post([FromBody] Kind kind)
+        {
+            if (kind.Betreuungsart.Length != 2 | kind.Betreuungsumfang.Length != 3)
+            {
+                return BadRequest();
+            }
+
+            foreach (Kind k in kindList)
+            {
+                if (k.SVN == kind.SVN)
+                {
+                    return BadRequest();
+                }
+            }
+
+            kind.KindId = kindList.Count +1;
+            foreach (Kind k in kindList)
+            {
+                if (k.KindId == kind.KindId)
+                {
+                    kind.KindId +=1;
+                }
+            }
+            kindList.Add(kind);
+
+            return CreatedAtAction("Get", new {id = kind.KindId});
+        }
+
+        // PUT api/<Kind>/5
+        [HttpPut("{id}")]
+        public Kind Put(int id, [FromBody] Kind kindVar)
+        {
+            var kind = kindList.Where(k => k.KindId == id).FirstOrDefault();
+            kind.Betreuungsart = kindVar.Betreuungsart;
+            kind.Betreuungsumfang = kindVar.Betreuungsumfang;
+            return kind;
+        }
+
+        // DELETE api/<Kind>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var kind = kindList.Where(k => k.KindId == id).FirstOrDefault();
+            kindList.Remove(kind);
+        }
+    }
+}
--- KindergartenWebServices/Models/Kind.cs
+++ KindergartenWebServices/Models/Kind.cs
@@ -1,32 +1,69 @@
-﻿namespace KindergartenWebServices.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KindergartenWebServices.Models
 {
     public class Kind
     {
+        //[Required]
         public int KindId { get; set; }
+
+        [Required]
+        [MinLength(2)] 
         public string Nachname { get; set; }
+        [Required]
+        [MinLength(2)]
         public string Vorname { get; set; }
+        [Required]
+        [MinLength(10)]
         public string SVN { get; set; }
+        [Required]
         public int Geburtstag { get; set; }
+        [Required]
         public int Geburtsmonat { get; set; }
+        [Required]
         public int Geburtsjahr { get; set; }
 
+        [Required]
+        [MinLength(2)]
         public string HauptwohnsitzAdresse { get; set; }
+        [Required]
+        [MinLength(4)]
         public string HauptwohnsitzPLZ { get; set; }
+        [Required]
+        [MinLength(2)]
         public string HauptwohnsitzOrt { get; set; }
 
+        [Required]
+        [MinLength(2)]
         public string ErzBerechtiger1Nachname { get; set; }
+        [Required]
+        [MinLength(2)]
         public string ErzBerechtiger1Vorname { get; set; }
+        [Required]
+        [MinLength(10)]
         public string ErzBerechtiger1SVN { get; set; }
         public string ErzBerechtiger2Nachname { get; set; }
         public string ErzBerechtiger2Vorname { get; set; }
         public string ErzBerechtiger2SVN { get; set; }
 
+        [Required]
+        [MinLength(2)]
         public string RechnungAdresse { get; set; }
+        [Required]
+        [MinLength(4)]
         public string RechnungPLZ { get; set; }
+        [Required]
+        [MinLength(2)]
         public string RechnungOrt { get; set; }
+        [Required]
+        [MinLength(5)] 
         public string EmailRechnung { get; set; }
 
+        [Required]
+        [MinLength(2)]
         public string Betreuungsart { get; set; }
+        [Required]
+        [MinLength(3)]
         public string Betreuungsumfang { get; set; }
 
         public float Familieneinkommen { get; set; }
