6e3ace8b6c7eb6658e1fd2e0f89ff5b0da854d41
Sonja Eisner <01113143@edu.campus02.at>
2022-11-26 11:11:45 +0100

    modify StaffelstufenController

--- KindergartenWebServices/Controllers/StaffelstufenController.cs
+++ KindergartenWebServices/Controllers/StaffelstufenController.cs
@@ -17,20 +17,6 @@ namespace KindergartenWebServices.Controllers
             _configuration = configuration;
         }
 
-        // GET: api/<StaffelstufenRechnerController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<StaffelstufenRechnerController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST api/<StaffelstufenRechnerController>
         [Route("/Staffelstufenberechnung")]
         [HttpPost]
@@ -58,18 +44,6 @@ namespace KindergartenWebServices.Controllers
             int staffelstufe = kind.Staffelstufe;
 
             return Ok(staffelstufe);
-        }
-
-        // PUT api/<StaffelstufenRechnerController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<StaffelstufenRechnerController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
